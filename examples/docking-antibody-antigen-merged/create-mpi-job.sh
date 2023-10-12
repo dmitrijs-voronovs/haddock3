eee5f3954614d3e44f22fda84236787319e0b506
Dmitrijs Voronovs <dmitrijs.voronovs@sapiens.com>
2023-10-12 17:14:29 +0200

    Fix script, update mpi examples

--- examples/docking-antibody-antigen-merged/create-mpi-job.sh
+++ examples/docking-antibody-antigen-merged/create-mpi-job.sh
@@ -1,7 +1,6 @@
 #!/bin/bash/
 
-filename="docking-antibody-antigen-CDR-accessible-mpi-"
-
+filename=$1
 cfg_template="template/${filename}template.cfg"
 job_template="template/${filename}template.job"
 
@@ -12,15 +11,15 @@ job_template_tasks_per_node="___TASKS_PER_NODE___"
 # script that accepts ncores, nodes, tasks_per_node as cli arguments and replaces them in template files
 
 # get cli arguments
-ncores=$1
-nodes=$2
-tasks_per_node=$3
-
-echo "ncores: $ncores, nodes: $nodes, tasks_per_node: $tasks_per_node"
+ncores=$2
+nodes=$3
+tasks_per_node=$4
 
 #create cfg and job files with ncores in filename
 cfg_file="${filename}${ncores}.cfg"
 job_file="${filename}${ncores}.job"
+
+echo "filename: $job_file, ncores: $ncores, nodes: $nodes, tasks_per_node: $tasks_per_node"
 
 # copy template files
 cp $cfg_template $cfg_file
@@ -30,5 +29,6 @@ cp $job_template $job_file
 sed -i "s/$cfg_template_ncores/$ncores/g" $cfg_file
 
 # replace nodes and tasks_per_node in job file
+sed -i "s/$cfg_template_ncores/$ncores/g" $job_file
 sed -i "s/$job_template_nodes/$nodes/g" $job_file
 sed -i "s/$job_template_tasks_per_node/$tasks_per_node/g" $job_file
