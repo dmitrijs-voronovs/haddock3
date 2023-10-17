b1cf2ab2cdd799be4c040b317d2bcb337e98d4f8
Dmitrijs Voronovs <dmitrijs.voronovs@sapiens.com>
2023-10-17 12:06:27 +0200

    Create job batches

--- examples/docking-antibody-antigen-merged/batch-daam-mpi-2-nodes.sh
+++ examples/docking-antibody-antigen-merged/batch-daam-mpi-2-nodes.sh
@@ -9,8 +9,8 @@ job1_name=daam-mpi-4
 job2_name=daam-mpi-32
 job3_name=daam-mpi-64
 
-job1_id=$(sbatch --job-name="$job1_name" $job1 --restart=0 | awk '{print $NF}')
+job1_id=$(sbatch --job-name="$job1_name" $job1 | awk '{print $NF}')
 echo "jobid1: $job1_id"
-job2_id=$(sbatch --job-name="$job2_name" --dependency=afterany:$job1_id $job2 --restart=0  | awk '{print $NF}')
+job2_id=$(sbatch --job-name="$job2_name" --dependency=afterany:$job1_id $job2 | awk '{print $NF}')
 echo "jobid2: $job2_id"
-sbatch --job-name="$job3_name" --dependency=afterany:$job2_id $job3 --restart=0
+sbatch --job-name="$job3_name" --dependency=afterany:$job2_id $job3
--- examples/docking-antibody-antigen-merged/batch-daam-mpi.sh
+++ examples/docking-antibody-antigen-merged/batch-daam-mpi.sh
@@ -9,8 +9,8 @@ job1_name=daam-mpi-6
 job2_name=daam-mpi-36
 job3_name=daam-mpi-72
 
-job1_id=$(sbatch --job-name="$job1_name" $job1 --restart=0 | awk '{print $NF}')
+job1_id=$(sbatch --job-name="$job1_name" $job1 | awk '{print $NF}')
 echo "jobid1: $job1_id"
-job2_id=$(sbatch --job-name="$job2_name" --dependency=afterany:$job1_id $job2 --restart=0  | awk '{print $NF}')
+job2_id=$(sbatch --job-name="$job2_name" --dependency=afterany:$job1_id $job2 | awk '{print $NF}')
 echo "jobid2: $job2_id"
-sbatch --job-name="$job3_name" --dependency=afterany:$job2_id $job3 --restart=0
+sbatch --job-name="$job3_name" --dependency=afterany:$job2_id $job3
